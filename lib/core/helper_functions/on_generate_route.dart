bccbf4e9c39ab5c51da5ea50baa4c9caec202732
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-12-28 15:00:54 +0200

    splash logic done

--- lib/core/helper_functions/on_generate_route.dart
+++ lib/core/helper_functions/on_generate_route.dart
@@ -1,12 +1,16 @@
+import 'package:e_commerce_app/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:e_commerce_app/features/splash_feature/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
-    // Define your routes here
     case SplashView.routeName:
       return MaterialPageRoute(
         builder: (context) => const SplashView(),
+      );
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const OnBoardingView(),
       );
     default:
       return MaterialPageRoute(
