0450fd872be95596025c3eca212a629c471498b4
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-12-28 14:16:33 +0200

    Update main.dart to use SplashView and update test/widget_test.dart to use FruitsApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/splash_feature/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class FruitsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: SplashView(),
     );
   }
 }
