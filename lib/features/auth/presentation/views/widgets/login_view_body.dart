07479159227ab9bc6dd42b0c1ecb6c800560f38a
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-01-28 22:04:45 +0200

    create custom text form field and add it to login view body

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -5,11 +7,31 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Column(
-        children: [
-          // Add your login view widgets here
-        ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+              suffixIcon: Icon(
+                Icons.visibility,
+                color: Color(0xffC9CECF),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
