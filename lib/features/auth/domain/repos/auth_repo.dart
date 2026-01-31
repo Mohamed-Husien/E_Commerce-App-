b2dc9231ebc9e10a06046e615e0a2a36a9f38095
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-01-31 18:57:44 +0200

    create auth repo and create User entity

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,6 @@
+import 'package:e_commerce_app/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<UserEntity> createUserWithEmailAndPassword(
+      String email, String password);
+}
