c7466d888789cb17c3ab7df0fb9d0ead54e5c6cb
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-01-31 22:29:47 +0200

    add get it

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce_app/core/services/fire_base_auth_service.dart';
+import 'package:e_commerce_app/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:e_commerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetit() {
+  getIt.registerLazySingleton<FireBaseAuthService>(
+    () => FireBaseAuthService(),
+  );
+  getIt.registerLazySingleton<AuthRepo>(
+    () => AuthRepoImpl(
+      fireBaseAuthService: getIt<FireBaseAuthService>(),
+    ),
+  );
+}
