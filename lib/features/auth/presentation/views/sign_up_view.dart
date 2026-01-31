438f5a8da699a3c277cde1ec9d1bf0ecd9119868
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-01-31 23:13:59 +0200

    provide sign up cubit

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,15 +1,24 @@
+import 'package:e_commerce_app/core/services/get_it_service.dart';
 import 'package:e_commerce_app/core/widgets/custom_app_bar.dart';
+import 'package:e_commerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:e_commerce_app/features/auth/presentation/manager/cubits/sign_up_cubit/sign_up_cubit.dart';
 import 'package:e_commerce_app/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
   static const String routeName = 'signUpView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: "حساب جديد"),
-      body: const SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignUpCubit(
+        getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: "حساب جديد"),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
