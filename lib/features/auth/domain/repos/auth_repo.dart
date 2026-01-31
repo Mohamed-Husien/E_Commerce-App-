b73b7a17c89b1dd697ef7632b87e78187eda68a4
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-01-31 20:54:47 +0200

    Added dartz and failure packages, updated auth_repo

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/core/errors/failures.dart';
 import 'package:e_commerce_app/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
       String email, String password);
 }
