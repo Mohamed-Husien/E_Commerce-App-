d4b07161d6136a528f7a0a71ee3d7f2668db8b21
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-02-01 23:00:48 +0200

    create cubit done

---
+++ lib/features/auth/presentation/manager/cubits/sign_in_cubit/sign_in_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_commerce_app/features/auth/domain/entities/user_entity.dart';
+import 'package:e_commerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_in_state.dart';
+
+class SignInCubit extends Cubit<SignInState> {
+  SignInCubit(this.authRepo) : super(SignInInitial());
+  final AuthRepo authRepo;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    emit(SignInLoading());
+    final result = await authRepo.signInWithEmailAndPassword(email, password);
+    result.fold(
+      (failure) => emit(
+        SignInFailure(failure.message),
+      ),
+      (user) => emit(
+        SignInSuccess(user),
+      ),
+    );
+  }
+}
