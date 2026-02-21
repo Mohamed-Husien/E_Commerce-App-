1f37a8185761ad04a5f781357119e51cdb3a4912
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-02-21 12:04:06 +0200

    home search text field done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/core/widgets/search_text_field.dart';
 import 'package:e_commerce_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +8,28 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: CustomHomeAppBar(),
-        ),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 16,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: CustomHomeAppBar(),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 16,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SearchTextField(),
+          )
+        ],
+      ),
     );
   }
 }
