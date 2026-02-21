b217d590c20a743d67c75b13c98d451331787cd8
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-02-21 14:10:31 +0200

    featured item done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/constants.dart';
 import 'package:e_commerce_app/core/widgets/search_text_field.dart';
 import 'package:e_commerce_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:e_commerce_app/features/home/presentation/views/widgets/featured_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -27,6 +28,14 @@ class HomeViewBody extends StatelessWidget {
           ),
           SliverToBoxAdapter(
             child: SearchTextField(),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 12,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: FeaturedItem(),
           )
         ],
       ),
